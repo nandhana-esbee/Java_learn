05eec2ea046d265913f0ecfad4591cd93b65c1e1
Nandhana SB <nandhanaesbee16@gmail.com>
2023-09-10 17:21:23 +0530

    Deleted collecting n

--- PlusMinus.java
+++ PlusMinus.java
@@ -37,7 +37,7 @@ public class PlusMinus {
     public static void main(String[] args) throws IOException {
         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
 
-        int n = Integer.parseInt(bufferedReader.readLine().trim());
+        //int n = Integer.parseInt(bufferedReader.readLine().trim());
 
         List<Integer> arr = Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
             .map(Integer::parseInt)
